374e08cc3827493d5a35aeeb3647373804636779
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-22 14:16:09 -0600

    updating insert statement new patient

--- web/new_patient.php
+++ web/new_patient.php
@@ -1,5 +1,7 @@
 <?php
 require('db_connection.php');
+error_reporting(E_ALL);
+ini_set('display_errors', true);
 ?>
 <?php
    $fname = $lname = $staddress = $birthdate = $doctor = $insurance = $notes = "";
