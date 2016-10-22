656875ccb4968bd2ec320dceb43f366b5ed47abb
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-22 11:10:02 -0600

    patient list

--- web/patient_list.php
+++ web/patient_list.php
@@ -14,40 +14,31 @@ require('db_connection.php');
         <main class="content">
               <div class="app">
                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/app_links.php'; ?>
-            </div>
-        <?php
-            echo 'Hello from patient list';
-        ?>
-        </main>
-    </body>
-</html>
-<!--
-
-<h4>Patient List</h4>
+               <h4>Patient List</h4>
             <select name="patient">
                 <option value="list">All Patients</option> 
                   <h2>Patient Information</h2>
                    <?php
-//                    $pquery = $db->query('SELECT * FROM patient')->fetchAll();
-//                
-//                    if($_SERVER["REQUEST_METHOD"] == "POST"){
-//                        $patient = $_POST['patient'];
-//                        if($patient != 'list'){
-//                            $pquery = $db->query("SELECT * FROM patient WHERE first_name='$patient'")->fetchAll();
-//                        }
-//                    }
-//
-//                    foreach($db->query('SELECT DISTINCT first_name FROM patient')->fetchAll() as $patient){
-//                        if($_SERVER["REQUEST_METHOD"] == "POST"){
-//                            if($_POST["patient"] == $patient["first_name"]){ 
-//                                $selected = "selected='selected'";
-//                            }
-//                            else{
-//                                $selected = "";
-//                            }
-//                        }
-//                        echo '<option value="' . $patient['first_name'] . '"' . $selected . '>' . $patient['first_name'] . '</option>';
-//                    }
+                    $pquery = $db->query('SELECT * FROM patient')->fetchAll();
+                
+                    if($_SERVER["REQUEST_METHOD"] == "POST"){
+                        $patient = $_POST['patient'];
+                        if($patient != 'list'){
+                            $pquery = $db->query("SELECT * FROM patient WHERE first_name='$patient'")->fetchAll();
+                        }
+                    }
+
+                    foreach($db->query('SELECT DISTINCT first_name FROM patient')->fetchAll() as $patient){
+                        if($_SERVER["REQUEST_METHOD"] == "POST"){
+                            if($_POST["patient"] == $patient["first_name"]){ 
+                                $selected = "selected='selected'";
+                            }
+                            else{
+                                $selected = "";
+                            }
+                        }
+                        echo '<option value="' . $patient['first_name'] . '"' . $selected . '>' . $patient['first_name'] . '</option>';
+                    }
                     ?>       
                     <input type="submit" value="Search"/>
                 </select>
@@ -70,22 +61,21 @@ require('db_connection.php');
                           </tr>
                         </thead>
                         <tbody>
--->
                     <?php
-//                    foreach($pquery as $rows){
-//                        echo '<tr>';
-//                        echo '<strong><td>' . $rows['patient_id'] . '</td><td>' . $rows['first_name'] . '</td><td>' . $rows['last_name'] . '</td><td>' . $rows['street_address'] . '</td><td>' . $rows['city'] . '</td><td>' . $rows['state'] . $rows['zipcode'] . '</td><td>' . $rows['phone_number'] . '</td><td>' . '</td><td>' . $rows['phone_number']. $rows['birthdate'] . '</td><td>' . $rows['notes'] . '</td><td>' .. $rows['notes'] . '</td><td>' .. $rows['insurance'] . '</td><td>' . $rows['physician'];
-//                        echo '</td></tr>';
-//                     }
+                    foreach($pquery as $rows){
+                        echo '<tr>';
+                        echo '<strong><td>' . $rows['patient_id'] . '</td><td>' . $rows['first_name'] . '</td><td>' . $rows['last_name'] . '</td><td>' . $rows['street_address'] . '</td><td>' . $rows['city'] . '</td><td>' . $rows['state'] . $rows['zipcode'] . '</td><td>' . $rows['phone_number'] . '</td><td>' . '</td><td>' . $rows['phone_number']. $rows['birthdate'] . '</td><td>' . $rows['notes'] . '</td><td>' . $rows['notes'] . '</td><td>' . $rows['insurance'] . '</td><td>' . $rows['physician'];
+                        echo '</td></tr>';
+                     }
                     ?>
-<!--
                     </tbody>
                     </table>
                     </div>
--->
-<!--                    <div id="scriptures">-->
-                        <?php 
-//                        include('index.php');
-                        ?>
-<!--                     </div>-->
+                </div>
+        <?php
+            echo 'Hello from patient list';
+        ?>
+        </main>
+    </body>
+</html>
                     
