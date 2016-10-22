8342120dfa849ee33c8e5b155e7a0a6d750cb855
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-22 12:16:57 -0600

    updated app links

--- web/modules/app_links.php
+++ web/modules/app_links.php
@@ -1,10 +1,19 @@
  
        
             <ul class="app_links">
+             <li><a href="/web/new_patient.php">New Patient</a></li>
+             <li><a href="/web/patient_list.php">Patient Information</a></li>
+             <li><a href="/web/doctor_list.php">Doctor Information</a></li>
+             <li><a href="/web/schedule.php">Schedule</a></li>
+             <li><a href="/web/appointment.php">Make Appointment</a></li>
+            </ul>
+<!--
+            <ul class="app_links">
              <li><a href="index.php?action=go_new_patient">New Patient</a></li>
              <li><a href="index.php?action=go_patient_list">Patient Information</a></li>
              <li><a href="index.php?action=go_doctor_list">Doctor Information</a></li>
              <li><a href="index.php?action=go_schedule">Schedule</a></li>
              <li><a href="index.php?action=go_appointment">Make Appointment</a></li>
             </ul>
+-->
 
