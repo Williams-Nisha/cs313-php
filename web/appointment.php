911ddbe8ac488ad5cfc4ad018e9eb59b90b96599
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-22 14:43:02 -0600

    added schedule and appointment pages

---
+++ web/appointment.php
@@ -0,0 +1,26 @@
+<?php
+require('db_connection.php');
+?>
+<html>
+     <head>
+        <title>
+            Appointment Setter App
+        </title>
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="../stylesheets/styles.css">
+    </head>
+    <body>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
+        <main class="content">
+              <div class="app">
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/app_links.php'; ?>
+                <div class="information">
+                </div>
+              </div>
+        <?php
+            echo 'Hello from appointment page';
+        ?>
+        </main>
+    </body>
+</html>
+        
