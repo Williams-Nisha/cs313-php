44386e13e2740dfe1e1710f72203de3e55a7c29b
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-22 12:12:38 -0600

    adding doctor list

--- web/doctor_list.php
+++ web/doctor_list.php
@@ -14,45 +14,37 @@ require('db_connection.php');
         <main class="content">
           <div class="app">
                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/app_links.php'; ?>
-          </div>
-        <?php
-            echo "Hello from doctor list";
-        ?>
-        </main>
-    </body>
-</html>
-<!--
-    <select name="doctor">
+             <div class="information">
+               <h4>Doctor List</h4>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                   <select name="doctor">
         <option value="all">All Doctors</option> 
              <h2>Doctor Information</h2>
--->
              <?php
-//                $query = $db->query('SELECT * FROM physician')->fetchAll();
-//                
-//                if($_SERVER["REQUEST_METHOD"] == "POST"){
-//                    $doctor = $_POST['doctor'];
-//                    if($doctor != 'all'){
-//                        $query = $db->query("SELECT * FROM physician WHERE first_name='$doctor'")->fetchAll();
-//                    }
-//                }
-//
-//                foreach($db->query('SELECT DISTINCT first_name FROM physician')->fetchAll() as $doctor){
-//                    if($_SERVER["REQUEST_METHOD"] == "POST"){
-//                        if($_POST["doctor"] == $doctor["first_name"]){ 
-//                            $selected = "selected='selected'";
-//                        }
-//                        else{
-//                            $selected = "";
-//                        }
-//                    }
-//                    echo '<option value="' . $doctor['first_name'] . '"' . $selected . '>' . $doctor['first_name'] . '</option>';
-//                }
+                $query = $db->query('SELECT * FROM physician')->fetchAll();
+                
+                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    $doctor = $_POST['doctor'];
+                    if($doctor != 'all'){
+                        $query = $db->query("SELECT * FROM physician WHERE first_name='$doctor'")->fetchAll();
+                    }
+                }
+
+                foreach($db->query('SELECT DISTINCT first_name FROM physician')->fetchAll() as $doctor){
+                    if($_SERVER["REQUEST_METHOD"] == "POST"){
+                        if($_POST["doctor"] == $doctor["first_name"]){ 
+                            $selected = "selected='selected'";
+                        }
+                        else{
+                            $selected = "";
+                        }
+                    }
+                    echo '<option value="' . $doctor['first_name'] . '"' . $selected . '>' . $doctor['first_name'] . '</option>';
+                }
                 ?>       
-<!--
                 <input type="submit" value="Search"/>
         </select>
-                   
-          <div class="information">
+            </form>
             <table>
                 <thead>
                     <tr>
@@ -64,21 +56,17 @@ require('db_connection.php');
                     </tr>
                 </thead>
                 <tbody>
--->
                 <?php
-//                    foreach($query as $row){
-//                        echo '<tr>';
-//                        echo '<strong><td>' . $row['physician_id'] . '</td><td>' . $row['first_name'] . '</td><td>' . $row['last_name'] . '</td><td>' . $row['phone_number'] . '</td><td>' . $row['specialty_id'];
-//                        echo '</td></tr>';
-//                     }
+                    foreach($query as $row){
+                        echo '<tr>';
+                        echo '<strong><td>' . $row['physician_id'] . '</td><td>' . $row['first_name'] . '</td><td>' . $row['last_name'] . '</td><td>' . $row['phone_number'] . '</td><td>' . $row['specialty_id'];
+                        echo '</td></tr>';
+                     }
                     ?>
-<!--
                 </tbody>
             </table>
-        </div>
-        <div id="scriptures">
--->
-            <?php 
-//            include('index.php');
-            ?>
-<!--         </div>-->
+              </div>
+          </div>
+        </main>
+    </body>
+</html>
