9007e2bed9403dda563fd47f5ac2045413228008
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-22 10:26:25 -0600

    new patient form

--- web/new_patient.php
+++ web/new_patient.php
@@ -1,30 +1,11 @@
 <?php
 require('db_connection.php');
 ?>
-<html>
-    <head>
-        <title>
-            Appointment Setter App
-        </title>
-              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-      <link rel="stylesheet" href="../stylesheets/styles.css">
-    </head>
-    <body>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
-            
-        <main class="content">
-          <div class="app">
-           <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/app_links.php'; ?>
-             <?php echo "Hello from new patient"; ?>
-            </div>
-        </main>
-    </body>
-</html>
 <?php
    $fname = $lname = $staddress = $birthdate = $doctor = $insurance = $notes = "";
    $fnameErr = $lnameErr = $staddressErr = $birthdateErr = $cityErr = $stateErr =  $phoneErr = $zipcodeErr = $notesErr = "";
-    
-    if (isset($_POST) && !empty($_POST)){
+
+       if (isset($_POST) && !empty($_POST)){
         if($_POST['form'] == 'patientform') {
             if(empty($_POST["first_name"])){
                 $fnameErr = "First name is required";
@@ -82,7 +63,6 @@ require('db_connection.php');
             }
         }
     }
-
     
             if($fnameErr == "" && $lnameErr == "" && $staddressErr == "" && $cityErr == "" && $stateErr == "" && $zipcodeErr == ""&& $phoneErr == "" && $birthdateErr == "" && $doctorErr == "" && $doctorErr == ""){
                 
@@ -96,4 +76,53 @@ require('db_connection.php');
                 $fnameErr = $lnameErr = $staddressErr = $birthdateErr = $cityErr = $stateErr =  $phoneErr = $zipcodeErr = $notesErr = "";
             }
 ?>
+<html>
+    <head>
+        <title>
+            Appointment Setter App
+        </title>
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="../stylesheets/styles.css">
+    </head>
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
+            
+        <main class="content">
+          <div class="app">
+           <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/app_links.php'; ?>
+             <form action="." method="post">
+                <input type="hidden" name="action" value="patient_form">
+                 <b>First Name: </b><span><?= $fnameErr;?></span><br>
+                  <input type="text" name="first_name" value="<?=$fname?>"><br><br>
+                  <b>Last Name: </b><span><?= $lnameErr;?></span><br>
+                  <input type="text" name="last_name" value="<?=$lname?>"><br><br>
+                  <b>Street Address: </b><span><?= $staddressErr;?></span><br>
+                  <input type="text" name="street_address" value="<?=$staddress?>"><br><br>
+                  <b>City: </b><span><?= $cityErr;?></span><br>
+                  <input type="text" name="city" value="<?=$city?>"><br><br>
+                  <b>State: </b><span><?= $stateErr;?></span><br>
+                  <input type="text" name="state" value="<?=$state?>"><br><br>
+                  <b>Zip Code: </b><span><?= $zipcodeErr;?></span><br>
+                  <input type="text" name="zipcode" value="<?=$zipcode?>"><br><br>
+                  <b>Phone Number: </b><span><?= $phoneErr;?></span><br>
+                  <input type="text" name="phoneNumber" value="<?=$phoneNumber?>"><br><br>
+                  <b>Birthdate: </b><span><?= $birthdateErr;?></span><br>
+                  <input type="text" name="birthdate" value="<?=birthdate?>"><br><br>   
+                  <b>Preferred Doctor: </b><span></span><br>
+                  <input type="text" name="pref_doctor" value="<?=$doctor?>"><br><br> 
+                  <b>Insurance Information: </b><span></span><br>
+                  <input type="text" name="insurance" value="<?=$insurance?>"><br><br>           
+                  <b>Notes: </b><span></span><br>
+                  <textarea cols="30" rows="4" name="notes"><?=$notes?></textarea>
+
+                  <input type="submit" value="Submit">
+                  <br><br>
+              </form>
+
+             <?php echo "Hello from new patient"; ?>
+            </div>
+        </main>
+    </body>
+</html>
+
 
