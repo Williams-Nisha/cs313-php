dcf60ff5aaaf51274fecf016d7c8c9e28174270e
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-22 19:02:58 -0600

    fixed foreach loop new patient

--- web/new_patient.php
+++ web/new_patient.php
@@ -6,7 +6,6 @@ require('db_connection.php');
 <?php
    $fname = $lname = $staddress = $birthdate = $doctor = $insurance = $notes = "";
    $fnameErr = $lnameErr = $staddressErr = $birthdateErr = $cityErr = $stateErr =  $phoneErr = $zipcodeErr = "";
-    echo 'Before post validation';
        if (isset($_POST) && !empty($_POST)){
         if($_POST['form'] == 'patient_form') {
             if(empty($_POST["first_name"])){
@@ -145,7 +144,7 @@ require('db_connection.php');
                         <?php
                     foreach($pquery as $rows){
                         echo '<tr>';
-                        echo '<strong><td>' . $rows['patient_id'] . '</td><td>' . $rows['first_name'] . '</td><td>' . $rows['last_name'] . '</td><td>' . $rows['street_address'] . '</td><td>' . $rows['city'] . '</td><td>' . $rows['state'] . $rows['zipcode'] . '</td><td>' . $rows['phone_number'] . '</td><td>' . $rows['birthdate'] . '</td><td>' . $rows['notes'] . '</td><td>' . $rows['insurance_id'] . '</td><td>' . $rows['physician_id'];
+                        echo '<strong><td>' . $rows['first_name'] . '</td><td>' . $rows['last_name'] . '</td><td>' . $rows['street_address'] . '</td><td>' . $rows['city'] . '</td><td>' . $rows['state'] . '</td><td>' . $rows['zipcode'] . '</td><td>' . $rows['phone_number'] . '</td><td>' . $rows['birthdate'] . '</td><td>' . $rows['notes'] . '</td><td>' . $rows['insurance_id'] . '</td><td>' . $rows['physician_id'];
                         echo '</td></tr>';
                      }
                 
