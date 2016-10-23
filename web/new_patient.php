5b2bca031aee08b6ec00f455fafdd73a9805d3a6
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-22 21:13:03 -0600

    updated files

--- web/new_patient.php
+++ web/new_patient.php
@@ -76,6 +76,7 @@ ini_set('display_errors', true);
                 (DEFAULT, '$fname', '$lname', '$staddress', '$city', '$state', '$zipcode', '$phoneNumber', '$birthdate', '$notes', (SELECT insurance_id FROM insurance WHERE name='$insurance'), (SELECT physician_id FROM physician WHERE first_name='$doctor'))");
                                
     echo "after insert statement";
+                        $pquery = $db->query("SELECT * FROM patient WHERE first_name='$fname'")->fetchAll();
             }
 ?>
 <html>
@@ -143,11 +144,14 @@ ini_set('display_errors', true);
                         </thead>
                         <tbody>
                     <?php
-//                    foreach($pquery as $rows){
-//                        echo '<tr>';
-//                        echo '<strong><td>' . $rows['patient_id'] . '</td><td>' . $rows['first_name'] . '</td><td>' . $rows['last_name'] . '</td><td>' . $rows['street_address'] . '</td><td>' . $rows['city'] . '</td><td>' . $rows['state'] . $rows['zipcode'] . '</td><td>' . $rows['phone_number'] . '</td><td>' . '</td><td>' . $rows['phone_number']. $rows['birthdate'] . '</td><td>' . $rows['notes'] . '</td><td>' .. $rows['notes'] . '</td><td>' . $rows['insurance'] . '</td><td>' . $rows['physician'];
-//                        echo '</td></tr>';
-//                     }
+                    foreach($pquery as $rows){
+                        echo '<tr>';
+                        echo '<strong><td>' . $rows['patient_id'] . '</td><td>' . $rows['first_name'] . '</td><td>' . $rows['last_name'] . '</td><td>' . $rows['street_address'] . '</td><td>' . $rows['city'] . '</td><td>' . $rows['state'] . $rows['zipcode'] . '</td><td>' . $rows['phone_number'] . '</td><td>' . '</td><td>' . $rows['phone_number']. $rows['birthdate'] . '</td><td>' . $rows['notes'] . '</td><td>' . $rows['notes'] . '</td><td>' . $rows['insurance'] . '</td><td>' . $rows['physician'];
+                        echo '</td></tr>';
+                     }
+                        $fname = $lname = $staddress = $city = $state = $zipcode= $phoneNumber= $birthdate = $doctor = $insurance = $notes = "";
+                        $fnameErr = $lnameErr = $staddressErr = $birthdateErr = $cityErr = $stateErr =  $phoneErr = $zipcodeErr = $notesErr = "";
+                
                     ?>
 
                     </tbody>
