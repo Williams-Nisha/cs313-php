fc5ad741c242390174f2511689d696e3f3acdd29
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-28 11:56:51 -0600

    debugging appointment page

--- web/appointment.php
+++ web/appointment.php
@@ -38,13 +38,13 @@ ini_set('display_errors', true);
                 $doctor = $_POST["doctor"];
             }
         }
-//            $timestamp = $adate . " " . $atime; 
-//            $hour = SELECT extract(hour from timestamp $timestamp);
-//            echo $hour;
     }
           
             if($fnameErr == "" && $lnameErr == "" && $adateErr == "" && $atimeErr == "" && $doctorErr ==  ""){
                 if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['form'] == 'appointment_form'){
+            $timestamp = $adate . " " . $atime; 
+            $hour = SELECT extract(hour from timestamp $timestamp);
+            echo $hour;
                     $db->exec("INSERT INTO appointment (appointment_id, appointment_date, physician_id, patient_id) VALUES 
                     (DEFAULT, '$adate', (SELECT physician_id FROM physician WHERE first_name='$doctor'), (SELECT patient_id FROM patient WHERE first_name='$fname'))");
 
