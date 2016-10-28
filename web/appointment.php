c6c5aa6fe2b21259a7db6e88675dcedb9f943945
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-27 22:47:08 -0600

    debugging appointment page

--- web/appointment.php
+++ web/appointment.php
@@ -132,12 +132,13 @@ ini_set('display_errors', true);
 //                    echo '</td></tr>';
 //                        }
 //            }
-                    
+                    foreach($pquery as $row){
                 foreach($db->query("SELECT * FROM appointment a JOIN patient p ON a.patient_id = p.patient_id WHERE a.patient_id='" . $row['$fname'] . "'") as $appointment){
                 echo '<tr><td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>';
                 echo '<td>' . $appointment['appointment_date'] . "</td><td>" . $appointment['appointment_date'] . "</td><td>" . $appointment['physician_id'] ;
                     }
                     echo '</td></tr>';
+                    }
                     ?>
                 </tbody>
             </table>
