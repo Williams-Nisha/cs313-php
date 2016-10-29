455781dce39a40a5298cd2dc06b35172f832d413
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-28 22:23:48 -0600

    debugging appointment page

--- web/appointment.php
+++ web/appointment.php
@@ -42,21 +42,33 @@ ini_set('display_errors', true);
           
             if($fnameErr == "" && $lnameErr == "" && $adateErr == "" && $atimeErr == "" && $doctorErr ==  ""){
               if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['form'] == 'appointment_form'){
-                   $pquery = $db->query("SELECT * FROM appointment a JOIN physician p ON a.physician_id = p.physician_id INNER JOIN schedule s ON s.physician_id = p.physician_id INNER JOIN patient pa ON pa.physician_id = p.physician_id WHERE physician_id =" . $doctor . ";")->fetchAll();
-                  var_dump($pquery);
-//                    
+//                   $db->query("SELECT * FROM appointment a JOIN physician p ON a.physician_id = p.physician_id INNER JOIN schedule s ON s.physician_id = p.physician_id INNER JOIN patient pa ON pa.physician_id = p.physician_id;") as $appointment)
+                $pquery = $db->query(
+                    'SELECT
+                        a.appointment_date
+                    ,   s.start_time
+                    ,   s.end_time
+                    ,   p.physician_id
+                    ,   p.first_name
+                    FROM appointment a INNER JOIN schedule s
+                    ON  a.physician_id = s.physician_id
+                    INNER JOIN physician p
+                    ON s.physician_id = p.physician_id
+                    WHERE p.first_name = "' . $doctor . ';')
+                        
+//                        
 //                foreach($pquery as $appointment){
+//                  foreach($db->query("SELECT * FROM appointment a JOIN patient p ON a.patient_id = p.patient_id WHERE a.patient_id='" . $row['$fname'] . "'") as $appointment){
 //                    $timestamp = $adate . " " . $atime; 
-//                    $hour = 'SELECT extract(hour from timestamp "' . $timestamp . '")';
+////                    $hour = 'SELECT extract(hour from timestamp "' . $timestamp . '")';
 //                    $subHours = 'extract(epoch from $timestamp' . '-' . $appointment['start_date'].')/3600';
-//                    var_dump($subHours);
 //                    if($pquery['$hour'] >= 'SELECT extract(hour from timestamp "'. $pquery['start_time'].'")' && $pquery['$hour']<= 'SELECT extract(hour from timestamp "'. $pquery['end_time'].'")'){
 //                        echo 'You are able to book appointment';
 //                    } else {
 //                        echo ' The appoinment is out of date range.';
-//                    }
+                    }
                       
-//                }
+                }
                   
                     $db->exec("INSERT INTO appointment (appointment_id, appointment_date, physician_id, patient_id) VALUES 
                     (DEFAULT, '$adate', (SELECT physician_id FROM physician WHERE first_name='$doctor'), (SELECT patient_id FROM patient WHERE first_name='$fname'))");
