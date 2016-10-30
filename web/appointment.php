be5a82175beacbd68be74c15228a5a910f399db8
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-29 20:42:27 -0600

    edited appointment foreach

--- web/appointment.php
+++ web/appointment.php
@@ -152,7 +152,7 @@ ini_set('display_errors', true);
                         echo '<tr>';
                         echo  '<td>'. $rows['first_name'] . ' ' . $rows['last_name'] . '</td><td>' . $rows['appointment_date']; 
                          echo $rows['physician_id'];
-                        foreach($db->query("SELECT * FROM appointment a INNER JOIN physician p ON a.physician_id = p.physician_id") as $doctor){
+                        foreach($db->query("SELECT * FROM appointment a INNER JOIN physician p ON a.physician_id = p.physician_id WHERE a.physician_id = p.physician_id ") as $doctor){
 //                            if($doctor['physician_id'] == $rows['physician_id']){
                         echo '</td><td>' . $doctor['first_name'] . ' '. $doctor['last_name'] . '</td></tr>';
                             break;
