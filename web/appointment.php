fd51e3cabd26b50be4bd7a669e2d7eb6eed55ab4
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-29 18:17:02 -0600

    debugging

--- web/appointment.php
+++ web/appointment.php
@@ -151,7 +151,7 @@ ini_set('display_errors', true);
                      foreach($sched_apts as $rows){
                         echo '<tr>';
                         echo  '<td>'. $rows['first_name'] . ' ' . $rows['last_name'] . '</td><td>' . $rows['appointment_date']; 
-                        foreach($db->query("SELECT * FROM appointment a INNER JOIN physician p ON a.physician_id = p.physician_id WHERE a.physician_id='" . $row['physician_id']. "'") as $doctor){
+                        foreach($db->query("SELECT * FROM appointment a INNER JOIN physician p ON a.physician_id = p.physician_id") as $doctor){
 //                            if($doctor['physician_id'] == $rows['physician_id']){
                         echo '</td><td>' . $doctor['first_name'] . ' '. $doctor['last_name'];
                            }
