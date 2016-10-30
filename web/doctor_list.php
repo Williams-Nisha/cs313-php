3cc9545e6f2e3973963a83d554b704e1344ea3fc
Williams-Nisha <nishamwilliams@yahoo.com>
2016-10-29 19:36:49 -0600

    debugging

--- web/doctor_list.php
+++ web/doctor_list.php
@@ -59,14 +59,13 @@ require('db_connection.php');
                     foreach($query as $row){
                         echo '<tr>';
                         echo '<td>' . $row['physician_id'] . '</td><td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td><td>' . $row['phone_number'];
-                            foreach($db->query("SELECT * FROM specialty s INNER JOIN physician p ON s.physician_id = p.physician_id WHERE s.physician_id='" . $row['physician_id'] . "'") as $specialty){
+                            foreach($db->query("SELECT * FROM specialty s INNER JOIN physician p ON s.specialty_id = p.specialty_id WHERE p.first_name='" . $row['first_name'] . "'") as $specialty){
                                 echo $specialty['name'];
                         echo '</td><td>' . $specialty['name'] . "</td></tr>";
                         break;
                     }
                     echo '</td></tr>';
                      }
-                    
                     ?>
                 </tbody>
             </table>
